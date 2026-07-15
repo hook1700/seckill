ad71389d1315338e3a57cd185337fbcff41ff326
v_zwhozhang <v_zwhozhang@tencent.com>
2026-07-15 16:47:47 +0800

    modify:è°mysql增加重试

--- internal/repo/mysql_repo.go
+++ internal/repo/mysql_repo.go
@@ -1,7 +1,8 @@
 package repo
 
 import (
-	"fmt"
+	"log"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -11,14 +12,19 @@ var db *gorm.DB
 
 func InitMySQL(dsn string, maxOpen, maxIdle int) {
 	var err error
-	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic(fmt.Sprintf("mysql connect failed: %v", err))
+	for i := 0; i < 10; i++ {
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err == nil {
+			sqlDB, _ := db.DB()
+			sqlDB.SetMaxOpenConns(maxOpen)
+			sqlDB.SetMaxIdleConns(maxIdle)
+			log.Println("mysql connected")
+			return
+		}
+		log.Printf("mysql not ready, retry %d: %v", i+1, err)
+		time.Sleep(2 * time.Second)
 	}
-
-	sqlDB, _ := db.DB()
-	sqlDB.SetMaxOpenConns(maxOpen)
-	sqlDB.SetMaxIdleConns(maxIdle)
+	panic("mysql connect failed after retries")
 }
 
 type SeckillOrder struct {
